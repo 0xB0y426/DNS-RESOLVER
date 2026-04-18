e8be01f8dc88e231619b8ea952fed0751d204918
Magnetron <132174647+0xB0y426@users.noreply.github.com>
2026-04-18 19:19:36 -0300

    Update dnsresolver.c

--- dnsresolver.c
+++ dnsresolver.c
@@ -8,9 +8,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    struct hostent *vitima = gethostbyname(argv[1]);
-
-    printf("DNS RESOLVER v1.0\nBy unoxy5\n==================================\n");
+    struct hostent *target = gethostbyname(argv[1]);
+    
     printf("Address: %s\n", inet_ntoa(*((struct in_addr *)vitima->h_addr_list)));
 
     return 0;
